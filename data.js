05184783cbca62cc3bddbb8045fb8371a7bd985b
Oktado Putra <oktado.putra@gmail.com>
2020-11-04 09:41:35 +0700

    first

---
+++ data.js
@@ -0,0 +1,6 @@
+
+export let namaHewan = ['Harimau', 'Jerapah','Kuda','Biawak','Rusa','Babi','Rubah']
+export let namaBuah = ['Nanas','Semangka','Jeruk','Belimbing',]
+
+
+    
---
+++ index.js
@@ -0,0 +1,10 @@
+import {namaHewan, namaBuah} from './data.js'
+
+let listHewan = document.getElementById("listHewan");
+let listBuah = document.getElementByIdde("listBuah");
+console.log(namaHewan);
+console.log(namaBuah);
+listHewan.innerHTML = namaHewan;
+listBuah.innerHTML = namaBuah;
+
+
